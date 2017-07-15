38176060eb2863a5649f962344487a0f5469bb48
ShaperNX <maletxa@gmail.com>
2017-07-14 21:18:38 -0400

    normalized values method made. need tests

--- sensor_file/domain/records.py
+++ sensor_file/domain/records.py
@@ -4,17 +4,18 @@ __author__ = 'Laptop$'
 __date__ = '2017-07-12$'
 __description__ = " "
 __version__ = '1.0'
+
 from abc import abstractmethod
 import datetime
-from typing import Dict,List
+from typing import Dict, List
 from collections import OrderedDict
-
-
+import re
 
 class Parameter(object):
     """
     basic implementation of a parameter
     """
+
     def __init__(self, param_name, unit):
         self.parameter = param_name
         self.unit = unit
@@ -24,10 +25,11 @@ class Record(object):
     """
     implementation of a basic record given by any kind of data file
     """
-    def __init__(self, record_date:datetime.datetime= None,
-                 parameter:str= None,
-                 parameter_unit:str= None,
-                 value= None):
+
+    def __init__(self, record_date: datetime.datetime = None,
+                 parameter: str = None,
+                 parameter_unit: str = None,
+                 value=None):
         self.record_date = record_date
         self._parameter = Parameter(parameter, parameter_unit)
         self.value = value
@@ -37,7 +39,7 @@ class Record(object):
         return self._parameter.parameter
 
     @parameter.setter
-    def parameter(self, value:str):
+    def parameter(self, value: str):
         self._parameter.parameter = value
 
     @property
@@ -45,7 +47,7 @@ class Record(object):
         return self._parameter.unit
 
     @parameter_unit.setter
-    def parameter_unit(self, value:str):
+    def parameter_unit(self, value: str):
         self._parameter.unit = value
 
 
@@ -55,6 +57,7 @@ class TimeSeriesRecords(Record):
     Values are stored as an OrderedDict : [(date1, value1),(date2, value2),...]
     """
     TimeSerieValue = Dict[datetime.datetime, str]
+
     def __init__(self,
                  records_date: datetime.datetime = None,
                  values: TimeSerieValue = None,
@@ -64,8 +67,8 @@ class TimeSeriesRecords(Record):
         if self.value is None:
             self.value = OrderedDict()
 
-    def set_time_serie_values(self,times:List[datetime.datetime], values:list):
-        for date,val in zip(times,values):
+    def set_time_serie_values(self, times: List[datetime.datetime], values: list):
+        for date, val in zip(times, values):
             self.value[date] = val
 
     def get_data_at_time(self, at_date: datetime.datetime) -> list:
@@ -74,13 +77,13 @@ class TimeSeriesRecords(Record):
         Record date or a list of all the Record for the given date
         :param at_date: datetime object corresponding to the needed Record"""
         date_val_return = []
-        if at_date in  self.value.keys():
+        if at_date in self.value.keys():
 
-            date_val_return.append([at_date,self.value[at_date]])
+            date_val_return.append([at_date, self.value[at_date]])
         elif at_date.date() in \
-            [dict_dates.date() for dict_dates in list(self.value.keys())]:
+                [dict_dates.date() for dict_dates in list(self.value.keys())]:
 
-            for dates,val in self.value.items():
+            for dates, val in self.value.items():
                 if dates.date() == at_date.date():
                     date_val_return.append([dates, val])
         return date_val_return
@@ -100,9 +103,9 @@ class TimeSeriesRecords(Record):
             first_record_date = self.get_data_at_time(first_date)[0][0]
             second_record_date = self.get_data_at_time(last_date)[0][0]
 
-            for dates,val in self.value.items():
+            for dates, val in self.value.items():
                 if dates >= first_record_date and dates <= second_record_date:
-                    returned_data.append([dates,val])
+                    returned_data.append([dates, val])
                 if dates > second_record_date:
                     break
         except IndexError:
@@ -110,18 +113,17 @@ class TimeSeriesRecords(Record):
         finally:
             return returned_data
 
-    def get_data_before_date(self,date_before:datetime.datetime)->list:
+    def get_data_before_date(self, date_before: datetime.datetime) -> list:
         first_date = self.start_date
         if date_before < first_date:
             first_date = date_before
-        return self.get_data_between(first_date,date_before)
+        return self.get_data_between(first_date, date_before)
 
-    def get_data_after_date(self, date_after:datetime.datetime) ->list:
+    def get_data_after_date(self, date_after: datetime.datetime) -> list:
         last_date = self.end_date
         if date_after > last_date:
             last_date = date_after
-        return self.get_data_between(date_after,last_date)
-
+        return self.get_data_between(date_after, last_date)
 
     @property
     def end_date(self):
@@ -131,16 +133,18 @@ class TimeSeriesRecords(Record):
     def start_date(self):
         return list(self.value.keys())[0]
 
+
 class ChemistryRecord(Record):
     """
     implementation of a Chemistry record. The main difference is that a chemetry record have a detection limit
     """
+
     def __init__(self, sampling_date: datetime.datetime = None,
                  parameter: str = None,
                  parameter_unit: str = None,
                  value: str = None,
-                 detection_limit:str = None,
-                 report_date:datetime.datetime=None):
+                 detection_limit: str = None,
+                 report_date: datetime.datetime = None):
         """
 
         :param sampling_date: date when the sample have been taken
@@ -151,27 +155,32 @@ class ChemistryRecord(Record):
         :param report_date: date of the analysis or report date if not sure
         """
         super().__init__(sampling_date, parameter, parameter_unit, value)
-        self.detection_limit = detection_limit
+        self.lower_detection_limit = detection_limit
         self.report_date = report_date
+
     @property
-    def normalized_value(self):
+    def normalized_value(self) -> float:
         """
-        method returning a float value normalized
+        method returning a float value normalized by some business rules
         :return:
         """
+        assert self.value is not None, "no value for the record"
+        normal_value = float('nan')
+        # transform trace, not detected, nd, n.d, ...
+        # by dividing the detection limit by 2
+        # if there is no detection limit, the result is NaN
+        if re.search(r"^tr.*|n.d.*|nan",self.value.lower()):
+            if self.lower_detection_limit is not None:
+                normal_value =  float(self.lower_detection_limit)/2.0
+        # transform values like <0.5 to 0.25 by divinding the result by two
+        elif re.search(r".*<.*",self.value):
+            normal_value = float(self.value.replace("<",''))
+        # the result is just a result
+        elif not re.search(r"^tr.*|n.d.*|.*<.*", self.value):
+            normal_value = float(self.value)
+        # todo : what to do with values above higher detection limit
+        return normal_value
 
 
 if __name__ == '__main__':
-    ts = TimeSeriesRecords()
-    dates = [datetime.datetime(2014,10,3,15,00),
-             datetime.datetime(2014, 10, 3, 15, 30),
-             datetime.datetime(2014, 10, 4, 15, 00),
-             datetime.datetime(2014, 10, 5, 15, 00),
-             datetime.datetime(2014, 10, 6, 15, 00),
-             datetime.datetime(2014, 10, 7, 15, 00)]
-    vals = list(range(len(dates)))
-    for i,j in zip(dates,vals):
-        print(i,j)
-    ts.set_time_serie_values(dates,vals)
-    print(ts.get_data_at_time(datetime.datetime(2014, 10, 3,15,30)))
-    print(ts.get_data_at_time(datetime.datetime(2014, 10, 3)))
+    print(float('nan').is_integer())
