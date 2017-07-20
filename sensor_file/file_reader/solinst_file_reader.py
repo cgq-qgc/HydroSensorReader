87c3d0f47b602ca9734610c14f9880d68d0e9fe4
gravalxa <xavier.malet2@canada.ca>
2017-07-20 15:50:47 -0400

    Solinst LEV WIP. have to get parameter, parameter unit and channel number and data.

--- sensor_file/file_reader/solinst_file_reader.py
+++ sensor_file/file_reader/solinst_file_reader.py
@@ -7,7 +7,7 @@ __version__ = '1.0'
 
 import datetime
 import warnings
-
+import re
 from sensor_file.file_reader.abstract_file_reader import PlateformReaderFile
 
 
@@ -38,23 +38,83 @@ class SolinstFileReader(PlateformReaderFile):
 class LEVSolinstFileReader(PlateformReaderFile):
     def __init__(self, file_name: str = None, header_length: int = 10):
         super().__init__(file_name, header_length)
+        self.date_times = self._get_date_list()
 
     def _read_file_header(self):
         """
         implementation of the base class abstract method
         """
-        pass
+        self._create_plateform()
 
     def _read_file_data(self):
         """
         implementation of the base class abstract method
         """
-        pass
+        self._get_data()
 
     def _read_file_data_header(self):
         """
         implementation of the base class abstract method
         """
+        pass
+
+    def _create_plateform(self):
+        self._site_of_interest.visit_date = self._create_visited_date()
+        self._site_of_interest.site_name = self._get_site_name()
+        self._site_of_interest.instrument_serial_number = self._get_serial_number()
+        self._site_of_interest.project_name = self._get_project_name()
+        self._site_of_interest.batterie_level = None
+        self._site_of_interest.model_number = None
+
+    def _create_visited_date(self) -> datetime:
+        _date = None
+        _time = None
+        for lines in self.file_content[:self._header_length]:
+            if re.search("^date.*",lines.lower()):
+                _date = lines.split(":")[1].replace(" ","")
+            if re.search(r"^time.*",lines.lower()):
+                _time = lines.split(" :")[1].replace(" ","")
+        self._update_header_lentgh()
+        to_datetime = datetime.datetime.strptime("{} {}".format(_date,_time),'%m/%d/%y %H:%M:%S')
+        return to_datetime
+    def _update_header_lentgh(self):
+        for i,lines in enumerate(self.file_content):
+            if re.search('^.data.*',lines.lower()):
+                self._header_length = i+1
+                break
+
+    def _get_instrument_info(self, regex_:str) ->str:
+        str_to_find = None
+        for lines in self.file_content:
+            if re.search(regex_, lines):
+                str_to_find = lines.split("=")[1]
+                break
+        return str_to_find
+
+    def _get_site_name(self) -> str:
+        return self._get_instrument_info(r".*[lL]ocation.*")
+
+    def _get_serial_number(self):
+        serial_string = self._get_instrument_info(r".*(S|s)erial.number.*")
+        serial_numb = re.split(r"[ -]",serial_string)[1]
+        return serial_numb
+
+    def _get_project_name(self):
+        return self._get_instrument_info(r".*(I|i)nstrument.number.*")
+
+    def _get_number_of_channels(self) ->int:
+        return int(self._get_instrument_info(r" *Channel *=.*"))
+
+
+    def _get_date_list(self) -> list:
+        datetime_list = []
+        for lines in self.file_content[self._header_length+1:-1]:
+            sep_line = lines.split(" ")
+            _date_time = datetime.datetime.strptime("{} {}".format(sep_line[0],sep_line[1]),'%Y/%m/%d %H:%M:%S.%f')
+            datetime_list.append(_date_time)
+        return datetime_list
+
+    def _get_data(self):
         pass
 
 
@@ -174,9 +234,11 @@ if __name__ == '__main__':
     teste_all = True
 
     if teste_all:
-        file_location = os.path.join(file_loc, "2029499_F7_NordChamp_PL20150925_2015_09_25.xle")
+        # file_location = os.path.join(file_loc, "2029499_F7_NordChamp_PL20150925_2015_09_25.xle")
+        file_location = os.path.join(file_loc,"2041929_PO-06_XM20170307_2017_03_07.lev")
+
         solinst_file = SolinstFileReader(file_location)
         solinst_file.read_file()
 
-        for time_serie in solinst_file.sites.records:
-            print(str(time_serie))
+        # for time_serie in solinst_file.sites.records:
+        #     print(str(time_serie))
