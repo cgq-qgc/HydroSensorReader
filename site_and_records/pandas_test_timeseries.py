47f628326ce70ea35df8801fffee85ef8d2db47d
x-malet <xavier.malet.1@ulaval.ca>
2018-04-03 22:14:32 -0400

    other tests with pandas

--- site_and_records/pandas_test_timeseries.py
+++ site_and_records/pandas_test_timeseries.py
@@ -1,25 +1,26 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-rng = pd.date_range('2011-01-01', periods=72, freq='D')
-rng2 = pd.date_range('2011-01-01', periods=350, freq='H')
+rng = pd.date_range('2011-02-01', periods=10, freq='H')
+rng2 = pd.date_range('2011-01-01', periods=9, freq='H')
 ts = pd.Series(np.random.randn(len(rng)), index=rng)
 ts_2 = pd.Series(np.random.randn(len(rng2)), index=rng2)
 
 
-
 df = pd.DataFrame(ts,columns=['col1'])
-
-print()
+# print((ts['2011-12-01':'2012-01-01']))
+print('col2' in df.keys())
+#print()
 
 f = plt.figure()
 # d = df.plot()
 df1 = pd.DataFrame(ts_2,columns=['teste'])
-
+#print((df.index[1]-df.index[0]))
+#print((df1.index[1]-df1.index[0]))
+#print((df.index[1]-df.index[0]) > (df1.index[1]-df1.index[0]))
 df2 = df1.combine_first(df)
-df2['new_col_3'] = df2['col1'].interpolate()
-df2.plot()
-
-print(df2.head(48))
-plt.show(block=True)
+#df2['new_col_3'] = df2['col1'].interpolate()
+print(type(df2.index.values))
+#print(df2.head(48))
+#plt.show(block=True)
