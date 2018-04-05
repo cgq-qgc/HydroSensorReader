3be7a503e045b5f7fbd68d61a1fb919d6ab6ec55
x-malet <xavier.malet.1@ulaval.ca>
2018-04-04 22:53:14 -0400

    WIP. get_date, get_data, get_header_content done. Overloading the plot method

--- file_reader/compagny_file_reader/hanna_file_reader.py
+++ file_reader/compagny_file_reader/hanna_file_reader.py
@@ -5,7 +5,9 @@ __date__ = '2017-07-16$'
 __description__ = " "
 __version__ = '1.0'
 import datetime
+import re
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 from file_reader.abstract_file_reader import TimeSeriesFileReader, date_list
@@ -14,9 +16,12 @@ from file_reader.abstract_file_reader import TimeSeriesFileReader, date_list
 class XLSHannaFileReader(TimeSeriesFileReader):
     def __init__(self, file_name: str = None, header_length: int = 10):
         super().__init__(file_name, header_length)
+        self.header_content = {}
 
     def read_file(self):
         self._date_list = self._get_date_list()
+        self._read_file_header()
+        self._read_file_data_header()
         self._read_file_data()
 
     @property
@@ -31,7 +36,12 @@ class XLSHannaFileReader(TimeSeriesFileReader):
         """
         implementation of the base class abstract method
         """
-        pass
+        for row in self.header_info:
+            if row[0] is None or row[0] in ['GENERAL INFORMATION', 'LOT INFORMATION', 'SETTINGS']:
+                pass
+            else:
+                key = re.sub('^ *', '', row[0])
+                self.header_content[key] = row[1]
 
     def _get_date_list(self) -> date_list:
         date_list = [d[0] for d in hanna_file.data_sheet[1:]]
@@ -48,18 +58,42 @@ class XLSHannaFileReader(TimeSeriesFileReader):
         self._site_of_interest.records = pd.DataFrame(data=values, columns=hanna_file.data_sheet[0][2:],
                                                       index=self._get_date_list())
 
+
     def _read_file_data_header(self):
         """
         implementation of the base class abstract method
         """
-        pass
+        self.sites.site_name = self.header_content['Lot Name']
+        self.sites.instrument_serial_number = self.header_content['Instrument Serial No.']
+        self.sites.visit_date = self.header_content['Started Date and Time']
+
+    def plot(self, *args, **kwargs):
+        fig, temp_axe = plt.subplots(figsize=(20, 10))
+        temp_axe.plot(self.records['Temp.[°C]'])
+        temp_axe.set_ylabel('Temp.[°C]')
+        ph_text = 'pH '
+        ph_axe = temp_axe.twinx()
+        ph_axe.plot(self.records[ph_text], color='orange', linestyle='--')
+        ph_axe.set_ylabel(ph_text)
+
+        do_text = 'D.O.[%]'
+        cond_axe = temp_axe.twinx()
+        cond_axe.plot(self.records[do_text], color='red')
+        cond_axe.set_ylabel(do_text)
+        cond_axe.spines["right"].set_position(("axes", 1.3))
+
+        orp_text = 'ORP[mV]'
+        orp_axe = temp_axe.twinx()
+        orp_axe.plot(self.records[orp_text], color='red')
+        orp_axe.set_ylabel(orp_text)
+        orp_axe.spines["right"].set_position(("axes", 1.5))
+
 
 
 
 if __name__ == '__main__':
     import os
     import pprint
-    import matplotlib.pyplot as plt
     path = os.getcwd()
     while os.path.split(path)[1] != "scientific_file_reader":
         path = os.path.split(path)[0]
@@ -69,18 +103,10 @@ if __name__ == '__main__':
     print(file)
 
     hanna_file = XLSHannaFileReader(file)
-    date_list = [d[0] for d in hanna_file.data_sheet[1:]]
-    time_list = [datetime.time(d[1].hour, d[1].minute, d[1].second) for d in hanna_file.data_sheet[1:]]
-    date_time = [datetime.datetime(d.year, d.month, d.day, t.hour, t.minute, t.second)
-                 for d, t in zip(date_list, time_list)]
-    values = [val[2:] for val in hanna_file.data_sheet[1:]]
     hanna_file.read_file()
-    print(len(values))
-    print(len(date_time))
-    pprint.pprint(date_time[:5], width=350)
-    pprint.pprint(hanna_file.data_sheet[0][2:], width=250)
-    df = pd.DataFrame(data=values, columns=hanna_file.data_sheet[0][2:], index=date_time)
-    print(df.head())
+    pprint.pprint(hanna_file.header_content, width=250)
+
+    print(hanna_file.records.head())
     print()
-    hanna_file.records.plot(subplots=True, title=file_name)
+    hanna_file.plot(subplots=True, title=hanna_file.sites.site_name)
     plt.show(block=True)
