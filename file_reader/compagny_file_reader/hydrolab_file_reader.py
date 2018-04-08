f2eb01ff9ca8de500325b2a2d914c996cbbc2def
x-malet <xavier.malet.1@ulaval.ca>
2018-04-08 16:23:53 -0400

    create HydrolabFileReader

---
+++ file_reader/compagny_file_reader/hydrolab_file_reader.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'X-Malet'
+__date__ = '2018-04-08'
+__description__ = " "
+__version__ = '1.0'
+
+from file_reader.abstract_file_reader import TimeSeriesFileReader, date_list
+
+
+class TXTHydrolabFileReader(TimeSeriesFileReader):
+
+    def __init__(self, file_name: str = None, header_length: int = 10):
+        super().__init__(file_name, header_length)
+
+    def _get_date_list(self) -> date_list:
+        return super()._get_date_list()
+
+    def plot(self, *args, **kwargs):
+        super().plot(*args, **kwargs)
+
+    def read_file(self):
+        super().read_file()
+
+    def _read_file_header(self):
+        super()._read_file_header()
+
+    def _read_file_data_header(self):
+        super()._read_file_data_header()
+
+    def _read_file_data(self):
+        super()._read_file_data()
