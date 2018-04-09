3283e4c5813c6aab2d409ea404a6d1e32e0074d5
x-malet <xavier.malet2@canada.ca>
2018-04-09 11:20:31 -0400

    _read_file_data method done. WIP : plot() method

--- file_reader/compagny_file_reader/hydrolab_file_reader.py
+++ file_reader/compagny_file_reader/hydrolab_file_reader.py
@@ -6,9 +6,18 @@ __description__ = " "
 __version__ = '1.0'
 
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
+import re
 from file_reader.abstract_file_reader import TimeSeriesFileReader, date_list
 
+DATA_HEADER = 'data_header'
+PROBE_ID = 'probe_id'
+LOG_FILE_NAME = 'Log File Name'
+SETUP_DATE = 'Setup Date (YYYY-MM-DD)'
+SETUP_TIME = 'Setup Time (HH:MM:SS)'
+
+_START_DATA_WO_DATES = 2
 
 class TXTHydrolabFileReader(TimeSeriesFileReader):
 
@@ -19,23 +28,31 @@ class TXTHydrolabFileReader(TimeSeriesFileReader):
     @property
     def data_as_list(self) -> list:
         datas = self.file_content[self.data_header_index + 3:]
-        return [i.split(',') for i in datas]
+        data_liste = [i.split(',') for i in datas if len(i.split(',')) > 1]
+        return data_liste
+
+    @property
+    def data_header(self) -> list:
+        return self.header_content[DATA_HEADER]
+
+    @data_header.setter
+    def data_header(self, value: list):
+        self.header_content[DATA_HEADER] = value
 
     def _get_date_list(self) -> date_list:
         dates = []
         for i in self.data_as_list:
-            dates.append(pd.Timestamp("{} {}".format(i[0], i[1])))
+            try:
+                dates.append(pd.Timestamp("{} {}".format(i[0], i[1])))
+            except IndexError:
+                pass
+
         return dates
 
-    def plot(self, *args, **kwargs):
-        super().plot(*args, **kwargs)
-
     def read_file(self):
-        self._read_file_header()
-        self._read_file_data_header()
-        self._get_date_list()
-        self._read_file_data()
-
+        self._set_data_header_index()
+        self._date_list = self._get_date_list()
+        super(TXTHydrolabFileReader, self).read_file()
 
     def _read_file_header(self):
         for row in self.file_reader.get_file_header:
@@ -45,7 +62,7 @@ class TXTHydrolabFileReader(TimeSeriesFileReader):
             except IndexError as i:
                 if 'hydrolab ms' in row[0].lower():
                     row = row[0].split(" ")
-                    self.header_content['probe_id'] = row[2]
+                    self.header_content[PROBE_ID] = row[2]
         self._set_site_info()
 
     def _set_site_info(self):
@@ -54,37 +71,61 @@ class TXTHydrolabFileReader(TimeSeriesFileReader):
         self._set_site_probe_id()
 
     def _set_site_probe_id(self):
-        self.sites.instrument_serial_number = self.header_content['probe_id']
+        self.sites.instrument_serial_number = self.header_content[PROBE_ID]
 
     def _set_site_name(self, ):
-        self.sites.site_name = self.header_content['Log File Name']
+        self.sites.site_name = self.header_content[LOG_FILE_NAME]
 
     def _set_site_visite_date(self):
-        self.sites.visit_date = pd.Timestamp("{} {}".format(
-            self.header_content['Setup Date (YYYY-MM-DD)'],
-            self.header_content['Setup Time (HH:MM:SS)']))
 
-    def _read_file_data_header(self):
+        self.sites.visit_date = pd.Timestamp("{} {}".format(
+            self.header_content[SETUP_DATE],
+            self.header_content[SETUP_TIME]))
+
+    def _set_data_header_index(self):
         for i in self.file_content:
             row = i.split(',')
-
             if row[0].lower() == '"date"':
                 break
             else:
                 self.data_header_index += 1
-        data_head = self.file_content[self.data_header_index].replace('"', '').split(' : ')
-        data_unit = self.file_content[self.data_header_index + 1].replace('"', '').split(' : ')
+
+    def _read_file_data_header(self):
+        data_head = self.file_content[self.data_header_index].replace('"', '').split(',')
+        data_unit = self.file_content[self.data_header_index + 1].replace('"', '').split(',')
         data_header = zip(data_head, data_unit)
-        self.header_content['data_header'] = ["{} ({})".format(i, j)
-                                              for i, j in data_header]
+        self.data_header = ["{} ({})".format(i, j) for i, j in data_header if i != '']
 
     def _read_file_data(self):
-        values = 0
-        self.sites.records = pd.DataFrame()
+        datas = []
+        # iterate through row
+        for row in self.data_as_list:
+            try:
+                row_content = []
+                # iterate through values
+                for val in [i.replace('"','') for i in row[_START_DATA_WO_DATES:]]:
+                    if val != '':
+                        if val not in ['#','NAN']:
+                            row_content.append(float(val))
+                        else:
+                            row_content.append(np.nan)
+                datas.append(row_content[:9])
+            except:
+                pass
+        self._site_of_interest.records = pd.DataFrame(data=datas,
+                                                      index=self._date_list,
+                                                      columns=self.data_header[_START_DATA_WO_DATES:])
+        print(self.records.dtypes)
+
+    def plot(self, *args, **kwargs):
+        fig, temp_axe = plt.subplots(figsize=(20, 10))
+
+        self.records.plot(*args, **kwargs)
 
 
 if __name__ == '__main__':
     import os
+    import pprint
 
     path = os.getcwd()
     while os.path.split(path)[1] != "scientific_file_reader":
@@ -97,9 +138,9 @@ if __name__ == '__main__':
     hydro_file = TXTHydrolabFileReader(file)
     hydro_file.read_file()
     print(hydro_file.sites)
-    # pprint.pprint(hanna_file.file_reader.get_file_header, width=250)
+    pprint.pprint(hydro_file.file_reader.get_file_header, width=250)
 
-    # print(hanna_file.records.head())
-    # print()
-    # hanna_file.plot(subplots=True, title=hanna_file.sites.site_name)
-    # plt.show(block=True)
+    print(hydro_file.records.head())
+    print(hydro_file.records.describe())
+    hydro_file.plot(subplots=True, title=hydro_file.sites.site_name)
+    plt.show(block=True)
