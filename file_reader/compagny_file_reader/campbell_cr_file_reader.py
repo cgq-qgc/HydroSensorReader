7b96e7cd17808cfe74af7ebedab8746da50920af
x-malet <xavier.malet2@canada.ca>
2018-04-09 14:16:45 -0400

    WIP on campbell file. data_header property done

--- file_reader/compagny_file_reader/campbell_cr_file_reader.py
+++ file_reader/compagny_file_reader/campbell_cr_file_reader.py
@@ -14,15 +14,25 @@ import pandas as pd
 
 from file_reader.abstract_file_reader import TimeSeriesFileReader, date_list
 
-
+VALUES_START = 4
 class DATCampbellCRFileReader(TimeSeriesFileReader):
-    def __init__(self, file_name: str = None, header_length: int = 10):
-        super().__init__(file_name, header_length)
+    def __init__(self, file_path: str = None, header_length: int = 4):
+        super().__init__(file_path, header_length)
+
+    @property
+    def data_header(self):
+        start_row = 1
+        column_name = [i.replace('"', '') for i in self.file_content[start_row].split(',')]
+        column_unit = [i.replace('"', '') for i in self.file_content[start_row + 1].split(',')]
+        column_agg = zip(column_name, column_unit)
+        header_col_def = ['{} ({})'.format(i, j) for i, j in column_agg]
+        return header_col_def
 
     def _read_file_header(self):
         """
         implementation of the base class abstract method
         """
+        print(self.data_header)
         pass
 
     def _read_file_data(self):
@@ -39,3 +49,26 @@ class DATCampbellCRFileReader(TimeSeriesFileReader):
 
     def _get_date_list(self) -> date_list:
         return super()._get_date_list()
+
+
+if __name__ == '__main__':
+    import os
+    import pprint
+
+    path = os.getcwd()
+    while os.path.split(path)[1] != "scientific_file_reader":
+        path = os.path.split(path)[0]
+    file_loc = os.path.join(path, 'file_example')
+    file_name = 'cr_file_example.dat'
+    file = os.path.join(file_loc, file_name)
+    print(file)
+
+    campbell_file = DATCampbellCRFileReader(file)
+    campbell_file.read_file()
+    print(campbell_file.sites)
+    pprint.pprint(campbell_file.file_reader.get_file_header, width=250)
+
+    # print(campbell_file.records.head())
+    # print(campbell_file.records.describe())
+    # campbell_file.plot()
+    # plt.show(block=True)
