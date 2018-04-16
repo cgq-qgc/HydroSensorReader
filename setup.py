9f41219c8a683ef0ab12f5a9a4caeb71b7d8b890
x-malet <xavier.malet.1@ulaval.ca>
2018-04-15 22:29:54 -0400

    try something for the setup.py file

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@
 __author__ = 'Laptop'
 __date__ = '2018-04-15'
 __description__ = " "
-__version__ = '1.7'
+__version__ = '1.7.1'
 
 import os
 from distutils.core import setup
 
+import pypandoc
 from setuptools import find_packages
+
+try:
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
 
 install_reqs = []
 
@@ -16,6 +22,7 @@ this_dir, this_filename = os.path.split(__file__)
 print(this_dir)
 DATA_PATH = os.path.join(this_dir, 'hydsensread', "file_example", '*.*')
 print(DATA_PATH)
+
 with open('requirements.txt', 'r') as f:
     for lines in f.readlines():
         install_reqs.append(lines.replace('\n', ''))
@@ -34,6 +41,8 @@ setup(name='HydroSensorReader',
       packages=find_packages(),
       package_data={'hydsensread': [DATA_PATH]},
       install_requires=install_reqs,
+      long_description=description,
+      long_description_content_type='text/x-rst',
       license='GPLv3',
       classifiers=['Development Status :: 3 - Alpha',
                    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
