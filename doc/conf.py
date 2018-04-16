60ddb7532eb34ddcef06de17c46f0e063e9f647f
x-malet <xavier.malet.1@ulaval.ca>
2018-04-16 10:38:36 -0400

    add doc file

---
+++ doc/conf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Laptop'
+__date__ = '2018-04-16'
+__description__ = " "
+__version__ = '1.0'
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
