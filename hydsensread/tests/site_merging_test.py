f45fd91805c239237602d5415fd83e7d1d689def
Xmalet <maletxa@gmail.com>
2019-07-25 21:40:46 -0400

    add test files for merge method in SensorPlateform class

---
+++ hydsensread/tests/site_merging_test.py
@@ -0,0 +1,11 @@
+import unittest
+from hydsensread.site_and_records import SensorPlateform, TimeSeriesRecords
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
