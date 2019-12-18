23327d2ea86f8ea90be54fbc8732c1aa57af7935
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-12-18 13:32:01 -0500

    Update __init__.py files

--- hydsensread/file_reader/__init__.py
+++ hydsensread/file_reader/__init__.py
@@ -8,10 +8,9 @@ __version__ = '1.0'
 from hydsensread.file_reader.abstract_file_reader import AbstractFileReader, GeochemistryFileReader, \
     TimeSeriesFileReader, \
     TimeSeriesGeochemistryFileReader
-from .compagny_file_reader import SolinstFileReader, \
-    DATCampbellCRFileReader, \
-    XLSHannaFileReader, \
-    XSLMaxxamFileReader
+from .compagny_file_reader import (
+    CSVSolinstFileReader, LEVSolinstFileReader, XLESolinstFileReader,
+    DATCampbellCRFileReader, XLSHannaFileReader, XSLMaxxamFileReader)
 try:
     from .web_page_reader import GNBWaterQualityStation, GNBCoreSamplesNTSMapSearchWebScrapper
 except:
--- hydsensread/file_reader/compagny_file_reader/__init__.py
+++ hydsensread/file_reader/compagny_file_reader/__init__.py
@@ -7,7 +7,8 @@ __version__ = '0.0.1'
 from .campbell_cr_file_reader import DATCampbellCRFileReader
 from .hanna_file_reader import XLSHannaFileReader
 from .maxxam_file_reader import XSLMaxxamFileReader
-from .solinst_file_reader import CSVSolinstFileReader, LEVSolinstFileReader, XLESolinstFileReader, SolinstFileReader
+from .solinst_file_reader import (
+    CSVSolinstFileReader, LEVSolinstFileReader, XLESolinstFileReader)
 from .what_csv_file_reader import WhatMeteorologicalDataFileReader
 from .what_csv_file_reader import WhatStreamAndLevelDataFileReader
 from .what_csv_file_reader import WhatWaterLevelDataFileReader
